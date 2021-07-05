e769589376067bb54dda5ccc387197a2c6e08d2a
ilhan <ilhangrn@hotmail.com>
2021-07-05 15:29:20 +0300

    solo is added

---
+++ solo_prune.py
@@ -0,0 +1,147 @@
+print("ig_solo")
+import tensorflow_model_optimization
+
+import tempfile
+import os
+
+import tensorflow as tf
+import numpy as np
+
+from tensorflow import keras
+'''
+guide for pruning
+https://www.tensorflow.org/model_optimization/guide/pruning
+
+In this tutorial, you will:
+
+1. Train a tf.keras model for MNIST from scratch.
+2. Fine tune the model by applying the pruning API and see the accuracy.
+3. Create 3x smaller TF and TFLite models from pruning.
+4. Create a 10x smaller TFLite model from combining pruning and post-training quantization.
+5. See the persistence of accuracy from TF to TFLite.
+
+'''
+####
+#
+# Train a model for MNIST without pruning
+#
+####
+
+# Load MNIST dataset
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Normalize the input image so that each pixel value is between 0 and 1.
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# Define the model architecture.
+model = keras.Sequential([
+  keras.layers.InputLayer(input_shape=(28, 28)),
+  keras.layers.Reshape(target_shape=(28, 28, 1)),
+  keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation='relu'),
+  keras.layers.MaxPooling2D(pool_size=(2, 2)),
+  keras.layers.Flatten(),
+  keras.layers.Dense(10)
+])
+
+# Train the digit classification model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(
+  train_images,
+  train_labels,
+  epochs=4,
+  validation_split=0.1,
+)
+
+####
+#
+# Evaluate baseline test accuracy and save the model for later usage.
+#
+####
+
+_, baseline_model_accuracy = model.evaluate(
+    test_images, test_labels, verbose=0)
+
+print('Baseline test accuracy:', baseline_model_accuracy)
+
+_, keras_file = tempfile.mkstemp('.h5')
+tf.keras.models.save_model(model, keras_file, include_optimizer=False)
+print('Saved baseline model to:', keras_file)
+
+####
+#
+# Fine-tune pre-trained model with pruning
+#
+####
+
+'''
+Defining the model
+
+You will apply pruning to the whole model and see this in the model summary.
+
+In this example, you start the model with 50% sparsity (50% zeros in weights) and end with 80% sparsity.
+
+In the comprehensive guide, you can see how to prune some layers for model accuracy improvements.
+https://www.tensorflow.org/model_optimization/guide/pruning/comprehensive_guide.md
+
+'''
+
+import tensorflow_model_optimization as tfmot
+
+prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+
+# Compute end step to finish pruning after 2 epochs.
+batch_size = 128
+epochs = 2
+validation_split = 0.1 # 10% of training set will be used for validation set. 
+
+num_images = train_images.shape[0] * (1 - validation_split)
+end_step = np.ceil(num_images / batch_size).astype(np.int32) * epochs
+
+# Define model for pruning.
+pruning_params = {
+      'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=0.50,
+                                                               final_sparsity=0.80,
+                                                               begin_step=0,
+                                                               end_step=end_step)
+}
+
+model_for_pruning = prune_low_magnitude(model, **pruning_params)
+
+# `prune_low_magnitude` requires a recompile.
+model_for_pruning.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model_for_pruning.summary()
+
+'''
+Fine tune with pruning for two epochs.
+
+tfmot.sparsity.keras.UpdatePruningStep is required during
+training, and tfmot.sparsity.keras.PruningSummaries provides
+logs for tracking progress and debugging.
+'''
+
+logdir = tempfile.mkdtemp()
+
+callbacks = [
+  tfmot.sparsity.keras.UpdatePruningStep(),
+  tfmot.sparsity.keras.PruningSummaries(log_dir=logdir),
+]
+  
+model_for_pruning.fit(train_images, train_labels,
+                  batch_size=batch_size, epochs=epochs, validation_split=validation_split,
+                  callbacks=callbacks)
+
+_, model_for_pruning_accuracy = model_for_pruning.evaluate(
+   test_images, test_labels, verbose=0)
+
+# For this example, there is minimal loss in test
+# accuracy after pruning, compared to the baseline.
+print('Baseline test accuracy:', baseline_model_accuracy) 
+print('Pruned test accuracy:', model_for_pruning_accuracy)
